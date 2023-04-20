0c3f5cbedbbc8f53a689de363a1c1e8cc4572963
Maisam Alam <maisam.alam@gmail.com>
2023-04-19 23:53:35 -0400

    Update api_keys.py

--- Python_API/api_keys.py
+++ Python_API/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "f72d8d0fab797d1f00d3655dd25902fe"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "5c36ac5c6ed04ed0a774385f26f19782"
+geoapify_key = ""
